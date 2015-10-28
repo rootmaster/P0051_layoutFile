b0816ba653d8641e7d8a33b5f5437bbebe8725ea
rootmaster <rootmaster.2.0@gmail.com>
2015-10-28 23:35:00 +0300

    create OnClickListener

--- app/src/main/java/com/example/rootmaster/p0051_layoutfile/MainActivity.java
+++ app/src/main/java/com/example/rootmaster/p0051_layoutfile/MainActivity.java
@@ -8,13 +8,36 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import static com.example.rootmaster.p0051_layoutfile.R.id.button;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText edittext;
+    Button button1, button2, button3, button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        edittext = (EditText) findViewById(R.id.editText2);
+        button1 = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edittext.setText("Pressed button 1");
+            }
+        });
+
+        button2.setOnClickListener(this);
+
        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,6 +49,22 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
+
+
+    }
+
+    public void pressButton3(View view){
+
+        edittext.setText("Pressed button 3");
+
+    }
+
+    public void pressButton4(View view){
+
+        edittext.setText("Don't press me!");
+        button4.setText("NO!");
+        button4.setBackgroundColor(677);
+
     }
 
     @Override
@@ -35,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
+        @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -48,5 +87,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        edittext.setText("Pressed button 2");
     }
 }
